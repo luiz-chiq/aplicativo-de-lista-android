ac17d153aa39b9d6da563e2f770463a3ede7de2e
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-26 20:50:23 -0300

    criacao da nova activity

--- app/src/main/java/ifsp/chiquetano/ListInputActivity.java
+++ app/src/main/java/ifsp/chiquetano/ListInputActivity.java
@@ -2,13 +2,34 @@ package ifsp.chiquetano;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListInputActivity extends AppCompatActivity {
 
+    private Button btnSave;
+
+    private Button btnCancel;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_input);
+
+        btnCancel = findViewById( R.id.btnCancel );
+        btnSave = findViewById( R.id.btnSave );
+
+
+        btnCancel.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListInputActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/ifsp/chiquetano/MainActivity.java
+++ app/src/main/java/ifsp/chiquetano/MainActivity.java
@@ -1,30 +1,23 @@
 package ifsp.chiquetano;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private ListView listView;
 
-    // atributos relativos aos objetos gráficos
-    private EditText txtTexto;
-    private Button btnAdiciona;
-    private ListView lista;
+    private ArrayList<String> items = new ArrayList<>();
 
-    // ArrayList com as strings
-    private ArrayList<String> textos = new ArrayList<>();
-
-    // Adapter da lista
-    ArrayAdapter<String> adaptador;
+    ArrayAdapter<String> arrayAdapter;
 
 
     @Override
@@ -33,82 +26,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ligando os atributos com os ID dos objetos na interface
-        btnAdiciona = findViewById( R.id.btnAdiciona );
-        lista = findViewById( R.id.lista );
+        button = findViewById( R.id.btnAddItem );
+        listView = findViewById( R.id.listView );
 
-        // definindo o escutador do botão
-        btnAdiciona.setOnClickListener( new EscutadorBotao() );
+        button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListInputActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        // criando o adaptador
-        adaptador = new ArrayAdapter<>(
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, items);
 
-                this,                                   // contexto da activity
-                android.R.layout.simple_list_item_1,    // o desenho de item já pronto
-                android.R.id.text1,                     // o ID do TextView dentro do item
-                textos                              );  // fonte dos dados (arraylist)
-
-        // configurar a lista para utilizar este objeto adaptador
-        lista.setAdapter( adaptador );
-
-        // configurar a lista com o escutador de cliques comuns
-        lista.setOnItemClickListener( new EscutadorLista() );
-
-        // configurar a lista para aceitar cliques longos
-        lista.setLongClickable( true );
-
-        // configurar a lista com o escutador de cliques longos
-        lista.setOnItemLongClickListener( new EscutadorLista() );
-    }
-
-
-    private class EscutadorBotao implements View.OnClickListener {
-
-        @Override
-        public void onClick(View view) {
-
-            String t;
-
-            // pega o texto digitado na caixa
-            t = txtTexto.getText().toString();
-
-            // adiciona no ArrayList
-            textos.add( t );
-
-            // avisando o adapter que uma nova string foi inserida
-            adaptador.notifyDataSetChanged();
-        }
-    }
-
-    private class EscutadorLista implements AdapterView.OnItemClickListener,
-            AdapterView.OnItemLongClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-            // O parâmetro i é o número do item clicado ...
-            Toast.makeText(MainActivity.this, "Clique comum: " + textos.get(i),
-                    Toast.LENGTH_SHORT).show();
-        }
-
-
-        @Override
-        public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-            // remover o item de índice i do ArrayList
-            textos.remove( i );
-
-            // avisar o adapter que o arraylist mudou
-            adaptador.notifyDataSetChanged();
-
-            // mensagem informativa
-            Toast.makeText(MainActivity.this, "Item apagado!", Toast.LENGTH_LONG).show();
-
-            // receita de bolo: retornar true, indicando que o evento foi tratado.
-            // se retornar false, vai querer tratar clique comum também
-            return true;
-        }
+        listView.setAdapter( arrayAdapter );
 
     }
+
 
 }
