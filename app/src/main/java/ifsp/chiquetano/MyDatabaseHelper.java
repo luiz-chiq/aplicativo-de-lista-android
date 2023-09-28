a56bceebe8b427de034d7df66d6272de3d0c7fdd
luiz-chiq <chiquetanoluiz@gmail.com>
2023-09-28 15:52:58 -0300

    criação da database

---
+++ app/src/main/java/ifsp/chiquetano/MyDatabaseHelper.java
@@ -0,0 +1,89 @@
+package ifsp.chiquetano;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATABASE_NAME = "List.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "items";
+    private static final String ID = "_id";
+    private static final String TEXT = "text";
+    private static final String COLOR = "color";
+
+    MyDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME +
+                " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                TEXT + " TEXT, " +
+                COLOR + " INTEGER);";
+        db.execSQL(query);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void addItem(String text, int color){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(TEXT, text);
+        cv.put(COLOR, color);
+        long result = db.insert(TABLE_NAME,null, cv);
+        if(result == -1){
+            Toast.makeText(context, "ERRO AO ADICIONAR", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public ArrayList getItems(){
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query, null);
+        }
+
+        ArrayList list = new ArrayList<String>();
+        if (cursor.getCount() == 0){
+            return list;
+        }
+
+        while (cursor.moveToNext()){
+//            Item item = new Item(
+//                    cursor.getInt(0),
+//                    cursor.getString(1),
+//                    cursor.getInt(2));
+//            list.add(item);
+            list.add(cursor.getString(1));
+        }
+        return list;
+    }
+
+    void deleteItem(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result = db.delete(TABLE_NAME, "_id=?", new String[]{id});
+        if(result == -1){
+            Toast.makeText(context, "ERRO AO DELETAR", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+}
